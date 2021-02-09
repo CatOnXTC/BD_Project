3fbdc4a77bba58b9037d6bf36eafb84bba586e9c
96peterharris <96peterharris@gmail.com>
2021-02-10 00:18:50 +0100

    Many changes and cleaning up

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, url_for, render_template, redirect, session, sessions
 import requests
-from functions import hashPassword, verifyPasswordHash, addLoggedUser, delLoggedUser, checkIfLoggedIn
+from functions import hashPassword, verifyPasswordHash, addLoggedUser, delLoggedUser, checkIfLoggedIn, checkIfUser
 import json
 import os
 import base64
@@ -11,23 +11,19 @@ print("elo")
 url = 'http://127.0.0.1:5001/'
 
 app = Flask(__name__)
-# app.secret_key = os.urandom(16) 
-app.secret_key = "a chuj"
+app.secret_key = os.urandom(16) 
 
-@app.route('/')
-def renderLoginPage():
-    if 'username' in session:
-        return escape(session['login'])
-    return render_template('LoginPage.html')
+@app.route('/',methods=['GET'])
+def home():
+    return render_template('loginPage.html')
 
-@app.route('/', methods =["POST"]) 
-@app.route('/getLoginUrl', methods=["POST"])
-def getLogin():
+@app.route('/',methods=['POST'])
+@app.route('/login',methods=['POST','GET'])
+def login():
     if request.method == "POST":
-        error = "Invalid credentials motherfuckers"
+        error = "Invalid credentials!!!"
         login = request.form["loginPage_login"]
         password = request.form["loginPage_password"]
-        
         if login == "" or password == "": 
             return render_template("LoginPage.html", error=error)
         else:
@@ -36,52 +32,51 @@ def getLogin():
                 responseJson = response.json()
                 isPasswordValid = verifyPasswordHash(responseJson['password'], password)
                 if isPasswordValid:
-                    session['login']=login 
-                    print(session)
                     addLoggedUser(login)
-                    return render_template("PatientPage.html")
+                    return redirect("patientPage")
                 else:
-                    return render_template("LoginPage.html", error=error)
-            else:
-                return render_template("LoginPage.html", error=error)
-       
+                    render_template("LoginPage.html", error=error)   
+    return render_template("LoginPage.html")
 
-@app.route('/getRegisterUrl', methods=["POST"])
-def getRegister():
+
+@app.route('/patientPage')
+def patientPage():
+    return render_template('PatientPage.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
     if request.method == "POST":
-        error = "Invalid register data motherfuckers"
-        login = request.form["register_pesel"]
+        error = "Invalid register data!!!"
+        login = request.form["register_login"]
+        name = request.form["register_name"]
+        surname = request.form["register_surname"]
         password = request.form["register_password"]
         passwordRep = request.form["register_password_rep"]
        
         if password!=passwordRep:
             return render_template("RegisterPage.html", error=error)
         
-        files = {"first_name" : "firstName1", "last_name" : "lastName1", "pesel" : login, "password" : hashPassword(password)}
-        response = requests.post('http://127.0.0.1:5000/api/users',
-                     data=json.dumps(files),
-                     headers={'Content-Type':'application/json'})
+
+        if(checkIfUser(login)) == True:
+            files = {"first_name" : name, "last_name" : surname, "pesel" : login, "password" : hashPassword(password)}
+            response = requests.post('http://127.0.0.1:5000/api/users',
+                    data=json.dumps(files),
+                    headers={'Content-Type':'application/json'})
+        else:
+            files = {"first_name" : name, "last_name" : surname, "username" : login, "password" : hashPassword(password)}
+            response = requests.post('http://127.0.0.1:5000/api/employees',
+                    data=json.dumps(files),
+                    headers={'Content-Type':'application/json'})  
 
         if response.status_code == 201:
-            addLoggedUser(login)
-            session['login']=login 
-            return render_template("PatientPage.html")
+            return redirect("login")
         else: 
             return render_template("RegisterPage.html", error=error)
-
-@app.route('/getRegisterUrl', methods=["GET", "POST"])
-def getRegisterUrl():
     return render_template('RegisterPage.html')
-
-@app.route('/getLoginUrl', methods=["GET", "POST"])
-def getLoginUrl():
-    return render_template('LoginPage.html')
-
-@app.route('/logOut', methods=["GET","POST"])
-def logOut():
-    delLoggedUser(session['login'])
-    session.pop('login',None)  
-    return redirect(url_for('getLoginUrl'))
+   
+@app.route('/logout', methods=['GET'])    
+def logout():
+    return redirect(url_for('login'))
 
 if __name__=='__main__': 
     app.run(port=5001) 
